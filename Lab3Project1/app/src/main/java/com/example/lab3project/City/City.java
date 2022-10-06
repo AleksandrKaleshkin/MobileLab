c97cca0a8ee4dba5a56db5e7b75554b783dd2e66
xa4ipur4ik <kirill.nowoselow@mail.ru>
2022-10-06 11:33:09 +0300

    06.10.22

---
+++ Lab3Project1/app/src/main/java/com/example/lab3project/City/City.java
@@ -0,0 +1,17 @@
+package com.example.lab3project.City;
+
+public class City {
+    private String Name;
+    private int size;
+
+    public City(String Name, int size)
+    {
+        this.Name=Name;
+        this.size=size;
+    }
+
+    public String getName(){return Name;}
+    public int getSize(){return size;}
+
+
+}
---
+++ Lab3Project1/app/src/main/java/com/example/lab3project/City/capital.java
@@ -0,0 +1,13 @@
+package com.example.lab3project.City;
+
+public class capital extends City{
+    public String yearOfcapital;
+
+
+
+    public capital(String Name, int size, String yearOfcapital)
+    {
+        super(Name,size,yearOfcapital);
+    }
+
+}
