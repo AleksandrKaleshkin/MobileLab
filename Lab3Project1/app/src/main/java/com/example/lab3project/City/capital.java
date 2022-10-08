9d7b5abde0154d8a429b6e00f3cf7b7d4985be02
xa4ipur4ik <kirill.nowoselow@mail.ru>
2022-10-08 13:02:23 +0300

    git commit -m ""

--- Lab3Project1/app/src/main/java/com/example/lab3project/City/capital.java
+++ Lab3Project1/app/src/main/java/com/example/lab3project/City/capital.java
@@ -1,13 +1,12 @@
 package com.example.lab3project.City;
 
 public class capital extends City{
+
     public String yearOfcapital;
 
 
-
-    public capital(String Name, int size, String yearOfcapital)
-    {
-        super(Name,size,yearOfcapital);
+    public capital(String Name, int size, String yearOfcapital) {
+        super(Name, size);
+        this.yearOfcapital=yearOfcapital;
     }
-
 }
---
+++ Lab3Project1/app/src/main/java/com/example/lab3project/City/regionalСenter.java
@@ -0,0 +1,11 @@
+package com.example.lab3project.City;
+
+public class regionalСenter extends City{
+
+    private String nameOfOblast;
+
+    public regionalСenter(String Name, int size,String nameOfOblast) {
+        super(Name, size);
+        this.nameOfOblast=nameOfOblast;
+    }
+}
