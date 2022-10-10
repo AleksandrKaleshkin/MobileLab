54cf8de940d39cac898cfe6476a216e0bc7031a8
xa4ipur4ik <kirill.nowoselow@mail.ru>
2022-10-10 12:41:20 +0300

    10/10/2022

--- Lab4Progect/app/src/main/java/com/example/lab4progect/MainActivity.java
+++ Lab4Progect/app/src/main/java/com/example/lab4progect/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.lab4progect;
 
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle savedlnstanceState) {
+        super.onCreate(savedlnstanceState);
         setContentView(R.layout.activity_main);
+        ImageView sun = findViewById(R.id.sun);
+        Animation sunRiseAnimation = AnimationUtils.loadAnimation(this, R.anim.sun_rise);
+        sun.startAnimation(sunRiseAnimation);
     }
+
+
 }
